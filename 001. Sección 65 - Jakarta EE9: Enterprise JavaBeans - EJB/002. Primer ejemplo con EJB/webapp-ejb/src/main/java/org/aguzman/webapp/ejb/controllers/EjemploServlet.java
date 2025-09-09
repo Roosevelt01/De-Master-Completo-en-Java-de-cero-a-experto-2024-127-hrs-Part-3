7c2feddbcdf88ed1bef9c0874380e686e6f9ce60
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-08 23:43:59 -0400

    java2

--- 001. Sección 65 - Jakarta EE9: Enterprise JavaBeans - EJB/002. Primer ejemplo con EJB/webapp-ejb/src/main/java/org/aguzman/webapp/ejb/controllers/EjemploServlet.java
+++ 001. Sección 65 - Jakarta EE9: Enterprise JavaBeans - EJB/002. Primer ejemplo con EJB/webapp-ejb/src/main/java/org/aguzman/webapp/ejb/controllers/EjemploServlet.java
@@ -10,15 +10,19 @@ import org.aguzman.webapp.ejb.service.ServiceEjb;
 
 import java.io.IOException;
 
-@WebServlet("/index")
+@WebServlet("/index")// Paso 1: Mapea este Servlet a la URL "/index".
 public class EjemploServlet extends HttpServlet {
 
-    @EJB
+    @EJB // Paso 2: Inyecta el EJB ServiceEjb.
     private ServiceEjb service;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+     throws ServletException, IOException {
+    
+        // Paso 3: Invoca el método del EJB y guarda el resultado en un atributo de la solicitud.
         req.setAttribute("saludo", service.saludar("andres"));
+        // Paso 4: Redirige la solicitud y la respuesta a la vista JSP.
         getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
 
     }
--- 001. Sección 65 - Jakarta EE9: Enterprise JavaBeans - EJB/002. Primer ejemplo con EJB/webapp-ejb/src/main/java/org/aguzman/webapp/ejb/service/ServiceEjb.java
+++ 001. Sección 65 - Jakarta EE9: Enterprise JavaBeans - EJB/002. Primer ejemplo con EJB/webapp-ejb/src/main/java/org/aguzman/webapp/ejb/service/ServiceEjb.java
@@ -2,11 +2,13 @@ package org.aguzman.webapp.ejb.service;
 
 import jakarta.ejb.Stateless;
 
-@Stateless
+@Stateless// Paso 1: Anotación obligatoria para marcar la clase como un EJB sin estado.
 public class ServiceEjb {
 
         public String saludar(String nombre){
+            // Paso 2: Un método simple que contiene la lógica de negocio.
             System.out.println("Imprimiendo dentro del ejb con instancia: "+this);
             return "Hola que tal "+nombre;
         }
 }
+
