9f1743e456598c347d4fb0aa3db3b7995537a686
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-09 01:04:53 -0400

    java2

--- 001. Sección 65 - Jakarta EE9: Enterprise JavaBeans - EJB/003. Ejemplo EJB @Stateless vs @Stateful/webapp-ejb/src/main/java/org/aguzman/webapp/ejb/service/ServiceEjb.java
+++ 001. Sección 65 - Jakarta EE9: Enterprise JavaBeans - EJB/003. Ejemplo EJB @Stateless vs @Stateful/webapp-ejb/src/main/java/org/aguzman/webapp/ejb/service/ServiceEjb.java
@@ -4,15 +4,15 @@ import jakarta.ejb.Stateful;
 import jakarta.ejb.Stateless;
 import jakarta.enterprise.context.RequestScoped;
 
-@RequestScoped//Paso 1
-@Stateful//Paso 2
+@RequestScoped // Paso 1: Vincula la instancia del EJB con el ciclo de vida de la solicitud HTTP (Request).
+@Stateful // Paso 2: Anotación que indica que este EJB mantendrá una conversación con un cliente.
 public class ServiceEjb {
 
-        private int contador;//Paso 3
+        private int contador; // Paso 3: Agrega un campo de instancia para mantener el estado.
 
         public String saludar(String nombre){
-            contador++;//Paso 4
-            System.out.println("\nValor del contado en método saludar " + contador);//Paso 4
+            contador++; // Paso 4: Incrementa el contador en cada llamada.
+            System.out.println("\nValor del contado en método saludar " + contador);
             System.out.println("\nImprimiendo dentro del ejb con instancia: "+this);
             return "Hola que tal "+nombre;
         }
