5fac580f671e5b569ac7eab1d8014473c42662cd
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-09 01:06:50 -0400

    java2

--- 001. Sección 65 - Jakarta EE9: Enterprise JavaBeans - EJB/003. Ejemplo EJB @Stateless vs @Stateful/webapp-ejb/src/main/java/org/aguzman/webapp/ejb/controllers/EjemploServlet.java
+++ 001. Sección 65 - Jakarta EE9: Enterprise JavaBeans - EJB/003. Ejemplo EJB @Stateless vs @Stateful/webapp-ejb/src/main/java/org/aguzman/webapp/ejb/controllers/EjemploServlet.java
@@ -14,17 +14,18 @@ import java.io.IOException;
 @WebServlet("/index")
 public class EjemploServlet extends HttpServlet {
 
-    @Inject//Paso 1
+    @Inject // Paso 1: Se cambia @EJB por @Inject para utilizar el contexto de CDI.
     private ServiceEjb service;
 
-    @Inject//Paso 2
-    private ServiceEjb service2;//Paso 3
+    @Inject // Paso 2: Se inyecta una segunda instancia del mismo EJB.
+    private ServiceEjb service2;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Paso 3: Compara las dos instancias.
         System.out.println("\nService si es igual a service2 = " + service.equals(service2));//Paso 4
         req.setAttribute("saludo", service.saludar("andres"));
-        req.setAttribute("saludo2", service2.saludar("john"));//Paso 5
+        req.setAttribute("saludo2", service2.saludar("john")); // Paso 4: Llama al método del segundo EJB.
         getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 }
