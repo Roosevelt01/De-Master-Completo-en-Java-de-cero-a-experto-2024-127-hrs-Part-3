c630132604bb3e91b93b6281311c5c00f1d9f5a0
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-11 11:18:27 -0400

    java2

--- 001. Sección 65 - Jakarta EE9: Enterprise JavaBeans - EJB/005.  La interfaz @Local/webapp-ejb/src/main/java/org/aguzman/webapp/ejb/service/ServiceEjbLocal.java
+++ 001. Sección 65 - Jakarta EE9: Enterprise JavaBeans - EJB/005.  La interfaz @Local/webapp-ejb/src/main/java/org/aguzman/webapp/ejb/service/ServiceEjbLocal.java
@@ -2,7 +2,7 @@ package org.aguzman.webapp.ejb.service;
 
 import jakarta.ejb.Local;
 
-@Local
+@Local // Paso 1: Anotación obligatoria que marca esta interfaz como local.
 public interface ServiceEjbLocal {
-    String saludar(String nombre);
+    String saludar(String nombre); // Paso 2: Define el método de negocio.
 }
